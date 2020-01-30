215a645b99de0675e06e42e00935956656ce0459
Jeff <jeffreyfang123@gmail.com>
2020-01-30 13:37:38 +0900

    Updated with basic query

---
+++ basic_query.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+from sodapy import Socrata
+
+client = Socrata("data.cityofnewyork.us", None)
+
+metadata = client.get_metadata("vx8i-nprf")
+
+count = client.get("vx8i-nprf", select="COUNT(*) as total")
+try 
+  count = int(count[0]["total"])
+except Exception 
+  print("Error in query, setting limit to default")
+  count = 25
+
+results = client.get("vx8i-nprf", limit = 10)
+# full = client.get("vx8i-nprf", limit = count)
+results_df = pd.DataFrame.from_records(results)
