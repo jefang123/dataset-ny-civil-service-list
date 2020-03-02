5dca88dd5abe65ff4c4669d978b39c12aa05315f
Jeff <jeffreyfang123@gmail.com>
2020-03-02 14:35:48 +0900

    set up basic React-Flask connection

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {};
+  componentDidMount = () =>
+    fetch('/time')
+      .then(res => res.json())
+      .then(data => {this.setState({time:data.time})})
+
   render() {
+    const state = this.state;
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +20,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>The current time is {state.time}</p>
       </div>
     );
   }
