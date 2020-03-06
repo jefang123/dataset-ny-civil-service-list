5340eea3d503597bea25fd6febdadbc347280c09
Jeff <jeffreyfang123@gmail.com>
2020-03-07 00:39:02 +0900

    Update css

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
 import logo from './logo.svg';
-import './App.css';
 
 class Home extends Component {
   state = {};
